f653a5a8046547c4c768f1fac154852aa20d3561
Suphasit Chiathamrongkiarti <6010110352@R301-mini09.local>
2020-08-26 19:57:32 +0700

    finish in class

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'MyList App',
+      title: 'MyList  App',
       routes: {
         AppRoutes.home: (context) => HomeScreen(),
         AppRoutes.edit: (context) => Edit(),
